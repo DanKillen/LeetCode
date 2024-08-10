4ff4f31d50fd289addc97ee1bb0eee82aa01103b
DanKillen <37762701+DanKillen@users.noreply.github.com>
2024-08-10 13:06:22 +0100

    ArrayDegreeFinder.java (needs finished)

---
+++ src/ArrayDegreeFinder.java
@@ -0,0 +1,20 @@
+//import java.util.Arrays;
+//import java.util.HashMap;
+//
+///**
+// * Created by Daniel Killen on 16/06/2024
+// * Given a non-empty array of non-negative integers nums,
+// * the degree of this array is defined as the maximum frequency of any one of its elements.
+// * Your task is to find the smallest possible length of a (contiguous) subarray of nums,
+// * that has the same degree as nums.
+// **/
+//public class ArrayDegreeFinder
+//{
+//   public static void main(String[] args) {
+//      System.out.println("Answer should be 2: " + findShortestSubArray(new int[]{1,2,2,3,1}));
+//      System.out.println("Answer should be 6: " + findShortestSubArray(new int[]{1,2,2,3,1,4,2}));
+//   }
+//   public static int findShortestSubArray(int[] nums) {
+//
+//   }
+//}
